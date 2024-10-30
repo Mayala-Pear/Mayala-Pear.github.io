6769a42503178b573d5216a1f5910114c345eb80
Mayala-Pear <143555524+Mayala-Pear@users.noreply.github.com>
2024-10-29 18:36:37 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,28 +1,17 @@
+const path = require('path');
 const express = require('express');
-const stripe = require('stripe')('sk_live_...'); // Replace with your Stripe secret key
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
 const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
 
-app.post('/create-payment-intent', async (req, res) => {
-    const { amount } = req.body; // Amount in cents
-    try {
-        const paymentIntent = await stripe.paymentIntents.create({
-            amount: amount,
-            currency: 'usd', // or your desired currency
-        });
-        res.send({
-            clientSecret: paymentIntent.client_secret,
-        });
-    } catch (error) {
-        res.status(500).send({ error: error.message });
-    }
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define a route for your HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
